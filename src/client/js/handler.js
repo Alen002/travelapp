6663f8bd9b1f5825e9e06f7a72956464a8a10019
Alen <alechkr@gmail.com>
2020-06-08 16:46:13 +0200

    fetch data from geonames.org implemented

--- src/client/js/handler.js
+++ src/client/js/handler.js
@@ -1,38 +1,44 @@
-/*** Global Variables ***/
+/**** Global Variables ****/
 // Geonames.org variables
 const apiGeonamesURL = 'http://api.geonames.org/'
 const apiGeonamesUser = process.env.APIGEONAMES;
-const apiGeonamesPath = `${apiGeonamesURL}postalCodeSearchJSON?placename=raleigh&username=${apiGeonamesUser}`;
 
-// Fetch results from the APIs are posted to the server
+// **** Fetch results from the APIs are posted to the server ****
 function handleSubmit(event) {
-    event.preventDefault()
-    // check what text was put into the form field
+    event.preventDefault();
     let cityName = document.getElementById('city-name').value
-    // User validation
+    // Input validation
     if (cityName.length == 0) {alert('Please enter a city');} 
 
-    console.log(geonames(apiGeonamesPath));
-
+    // Fetch data from geonames.org
+    const apiGeonamesPath = `${apiGeonamesURL}postalCodeSearchJSON?placename=${cityName}&username=${apiGeonamesUser}`;
+    geonames(apiGeonamesPath)
+    .then(data => {
+        console.log('Output on client: ',data.postalCodes[0]);
+        document.getElementById('geo-longitude').innerHTML= data.postalCodes[0].lat;
+        document.getElementById('geo-latitude').innerHTML= data.postalCodes[0].lng;
+        document.getElementById('geo-country').innerHTML= data.postalCodes[0].countryCode;
+    });
+    
+    
+    // Process finished
     console.log('The following city was entered:', cityName);
     console.log("::: City has been submitted :::");
 };
 
-
-// Fetch from geonames.org
-async function geonames(url) {
-   
+// **** Fetch geonames.org ****
+const geonames = async (url) => {
     try {
         const response = await fetch(url);
-        const cityData = await response.json();  
-        console.log(cityData);
+        const cityData = await response.json();
         return cityData;
     } 
         catch (error) {
         console.log(error);
     }
-}
+};
 
+// **** Fetch Weatherbit.io ****
 
 
 
